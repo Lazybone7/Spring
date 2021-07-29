7e80d16744db2243e14515ae1ea5a7d7921007a2
hqh <15757256089@163.com>
2021-07-29 15:02:50 +0800

    完善

--- SpringFirstExperience/src/main/java/com/ithan/ui/Client.java
+++ SpringFirstExperience/src/main/java/com/ithan/ui/Client.java
@@ -1,10 +1,17 @@
 package com.ithan.ui;
 import com.ithan.service.IAccountService;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
+/**
+ * ApplicationContext： 立即加载(常用) 采用此接口
+ * BeanFactory： 延迟加载
+ */
 public class Client {
     public static void main(String[] args) {
         //1. 获取核心容器对象
@@ -20,5 +27,14 @@ public class Client {
         for (int i = 0; i < 6; i++) {
             System.out.println(accountService);
         }
+
+
+        /*---------BeanFactory-------------------*/
+        /*
+        Resource resource = new ClassPathResource("bean.xml");
+        BeanFactory beanFactory = new XmlBeanFactory(resource);
+        IAccountService accountService = beanFactory.getBean("accountService",IAccountService.class);
+        accountService.saveAccount();
+        */
     }
 }
