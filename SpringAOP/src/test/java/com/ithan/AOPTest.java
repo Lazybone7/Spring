f36e4a0d1cf458134ca4783961b60a3c8931b234
han <1098333128@qq.com>
2021-08-09 00:36:51 +0800

    切入点表达式

---
+++ SpringAOP/src/test/java/com/ithan/AOPTest.java
@@ -0,0 +1,18 @@
+package com.ithan;
+
+
+import com.ithan.service.IAccountService;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AOPTest {
+    @Test
+    public void testAop(){
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
+        IAccountService accountService = context.getBean("accountService", IAccountService.class);
+        accountService.saveAccount();
+        accountService.deleteAccount();
+        accountService.updateAccount(10);
+    }
+}
